9bd84b9baaa82d35a04acf2cc9d7b02a6f1d39e8
NikitaYasiulevich <yasiulevichnikita@gmail.com>
2024-06-23 17:54:46 +0300

    Add functionality to add and edit shop items on ShopItemActivity, updated app icon and splash screen, and refreshed UI colors

--- app/src/main/java/com/nikitayasiulevich/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/nikitayasiulevich/shoppinglist/presentation/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.nikitayasiulevich.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
+        }
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
 
@@ -66,7 +72,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         shopListAdapter.onShopItemClickListener = {
-            //TODO
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
